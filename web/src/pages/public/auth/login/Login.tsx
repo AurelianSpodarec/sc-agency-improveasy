e0041b533c4b4874451b816d4fa16597ae69901a
Oliver <oli@silverchip.com>
2022-09-13 09:31:09 +0100

    remove LoginForm

--- web/src/pages/public/auth/login/Login.tsx
+++ web/src/pages/public/auth/login/Login.tsx
@@ -1,17 +1,82 @@
 import { Container, Section } from '@components/ui';
 import MainPublic from '@pages/public/_components/MainPublic';
 import Title from 'lib/src/components/typography/Title';
-import LoginForm from './LoginForm';
+import ActionButton from 'lib/src/components/button/ActionButton';
+import Form from 'lib/src/components/form/Form';
+import FormRow from 'lib/src/components/form/FormRow';
+import TextInput from 'lib/src/components/form/TextInput';
+import { Link } from 'react-router-dom';
+import useLogin from './hooks/useLogin';
 
-const Login: React.FC = () => (
-    <MainPublic>
-        <Section>
-            <Container>
-                <Title>Login</Title>
-                <LoginForm />
-            </Container>
-        </Section>
-    </MainPublic>
-);
+const Login: React.FC = () => {
+    const {
+        formState: { email, password },
+        handleChange,
+        handleSubmit,
+        isPosting,
+        customValidate,
+        error,
+        showConfirmEmail,
+        resendEmail,
+    } = useLogin();
+
+    return (
+        <MainPublic>
+            <Section>
+                <Container>
+                    <Title>Login</Title>
+                    <Form onSubmit={handleSubmit} isPosting={isPosting} error={error} omitButtons>
+                        <FormRow>
+                            <TextInput
+                                name="email"
+                                type="email"
+                                value={email}
+                                label="Email address"
+                                onChange={handleChange}
+                                required
+                            />
+                        </FormRow>
+                        <FormRow>
+                            <TextInput
+                                name="password"
+                                value={password}
+                                label="Password"
+                                onChange={handleChange}
+                                type="password"
+                                required
+                                customValidate={customValidate}
+                            />
+                        </FormRow>
+                        <p>
+                            <Link to="/auth/forgot-password">Forgot password?</Link>
+                        </p>
+                        <div className="d-flex justify-between">
+                            <p>
+                                Don't have an account? <Link to="/auth/register">Register</Link>
+                            </p>
+                            <ActionButton icon="sign-in" isPosting={isPosting}>
+                                Login
+                            </ActionButton>
+                        </div>
+
+                        {showConfirmEmail && (
+                            <>
+                                <p>
+                                    Your account is not comfirmed, please click the link in your
+                                    email.
+                                </p>
+                                <p>
+                                    <button type="button" onClick={resendEmail}>
+                                        Resend confimation.
+                                    </button>
+                                </p>
+                            </>
+                        )}
+                    </Form>
+                </Container>
+            </Section>
+        </MainPublic>
+    );
+};
 
 export default Login;
