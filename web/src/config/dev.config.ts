85313af29e17c0fc9ee0e5e760060c86b46f1426
Oliver <oli@silverchip.com>
2022-09-13 09:01:49 +0100

    fix config

--- web/src/config/dev.config.ts
+++ web/src/config/dev.config.ts
@@ -1,7 +1,8 @@
 import { Config } from '.';
 
 const config: Config = {
-    API_URL: 'https://localhost:5011',
+    // API_URL: 'https://localhost:5011',
+    API_URL: 'https://improveasy-api.madeatsilverchip.com',
 };
 
 export default config;
