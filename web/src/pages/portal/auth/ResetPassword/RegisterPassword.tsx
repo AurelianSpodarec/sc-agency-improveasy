27d4b3576462a1beeb4fc2c57c3ddaec3a8eeec6
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-09-14 22:35:13 +0100

    update/(put reset password into auth portal

--- web/src/pages/portal/auth/ResetPassword/RegisterPassword.tsx
+++ web/src/pages/portal/auth/ResetPassword/RegisterPassword.tsx
@@ -1,10 +1,49 @@
-function RegisterPassword() {
-    return (
-        <div>
-            <div></div>
-            RegisterPassword
-        </div>
-    );
-}
+import Title from 'lib/src/components/typography/Title';
+import Form from 'lib/src/components/form/Form';
+import FormRow from 'lib/src/components/form/FormRow';
+import TextInput from 'lib/src/components/form/TextInput';
 
-export default RegisterPassword;
+import MainPublic from '@pages/public/_components/MainPublic';
+import useResetPassword from './hooks/useResetPassword';
+import { Container, Section } from '@components/ui';
+import AuthCard from '../_components/authCard';
+import AuthHeader from '../_components/authHeader';
+
+const ResetPassword: React.FC = () => {
+    const {
+        isPosting,
+        error,
+        formState: { newPassword },
+        handleChange,
+        showSuccess,
+        handleSubmit,
+    } = useResetPassword();
+
+    return (
+        <AuthCard>
+            <AuthHeader title="Reset password" />
+            {/* {showSuccess ? (
+                <p>
+                    <p>Success!</p>
+                    Your password has been reset.
+                    {/* <Link to="/auth/login">Login</Link> */}
+            {/* </p> */}
+            {/* // ) : ( */}
+
+            <Form onSubmit={handleSubmit} isPosting={isPosting} error={error}>
+                <FormRow>
+                    <TextInput
+                        name="newPassword"
+                        value={newPassword}
+                        label="New Password"
+                        onChange={handleChange}
+                        type="password"
+                        required
+                    />
+                </FormRow>
+            </Form>
+        </AuthCard>
+    );
+};
+
+export default ResetPassword;
--- web/src/routes/AuthRoutes.tsx
+++ web/src/routes/AuthRoutes.tsx
@@ -3,6 +3,8 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import Login from '@pages/portal/auth/Login/Login';
 import Register from '@pages/portal/auth/Register/Register';
 import ForgotPassword from '@pages/portal/auth/ForgotPassword/ForgotPassword';
+import ResetPassword from '@pages/portal/auth/ResetPassword/RegisterPassword';
+import ConfirmEmail from '@pages/portal/auth/ConfirmEmail/ConfirmEmail';
 
 const AuthRoutes = () => {
     const { path } = useRouteMatch();
@@ -17,13 +19,13 @@ const AuthRoutes = () => {
             <Route exact path={`${path}/forgot-password`}>
                 <ForgotPassword />
             </Route>
-            {/* 
+
             <Route exact path={`${path}/reset-password/:token`}>
                 <ResetPassword />
             </Route>
             <Route exact path={`${path}/confirm-email/:token`}>
                 <ConfirmEmail />
-            </Route> */}
+            </Route>
         </Switch>
     );
 };
