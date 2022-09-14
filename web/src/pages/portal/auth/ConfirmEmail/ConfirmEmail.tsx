44c66e1935149cb475b984cedabab9fdb9b1aac3
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-09-14 22:31:01 +0100

    feat/(forgot password) - Put forgot password into its own component in portal

--- web/src/pages/portal/auth/ConfirmEmail/ConfirmEmail.tsx
+++ web/src/pages/portal/auth/ConfirmEmail/ConfirmEmail.tsx
@@ -1,10 +1,21 @@
-function ConfirmEmail() {
+import Form from 'lib/src/components/form/Form';
+
+import useConfirmEmail from './hooks/useConfirmEmail';
+
+import AuthCard from '../_components/authCard';
+import AuthHeader from '../_components/authHeader';
+
+const ConfirmEmail: React.FC = () => {
+    const { isPosting, error } = useConfirmEmail();
+
     return (
-        <div>
-            <div></div>
-            ConfirmEmail
-        </div>
+        <AuthCard>
+            <AuthHeader title="Confirm Email" />
+            <Form onSubmit={() => {}} isPosting={isPosting} error={error} omitButtons>
+                {isPosting && <i className="fal fa-spinner fa-spin"></i>}
+            </Form>
+        </AuthCard>
     );
-}
+};
 
 export default ConfirmEmail;
--- web/src/pages/portal/auth/ForgotPassword/ForgotPassword.tsx
+++ web/src/pages/portal/auth/ForgotPassword/ForgotPassword.tsx
@@ -3,22 +3,39 @@ import Form from 'lib/src/components/form/Form';
 import FormRow from 'lib/src/components/form/FormRow';
 import TextInput from 'lib/src/components/form/TextInput';
 
-import useForgotPassword from './hooks/useForgotPassword';
 import AuthCard from '../_components/authCard';
 
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import useForm from 'lib/src/hooks/useForm';
+import usePrevious from 'lib/src/hooks/usePrevious';
+import { getAuthError, getAuthIsPosting, getAuthPostSuccess } from '@selectors/auth';
+import { postSendForgotPassword } from '@actions/auth';
+import AuthHeader from '../_components/authHeader';
+
 const ForgotPassword: React.FC = () => {
-    const {
-        formState: { email },
-        handleChange,
-        handleSubmit,
-        isPosting,
-        error,
-        showSuccess,
-    } = useForgotPassword();
+    const dispatch = useDispatch();
+    const isPosting = useSelector(getAuthIsPosting);
+    const postSuccess = useSelector(getAuthPostSuccess);
+    const error = useSelector(getAuthError);
+
+    const [showSuccess, setShowSuccess] = useState(false);
+
+    const [formState, handleChange] = useForm({
+        email: '',
+    });
+
+    const prevPostSuccess = usePrevious(postSuccess);
+    useEffect(() => {
+        if (!prevPostSuccess && postSuccess) {
+            setShowSuccess(true);
+        }
+    }, [postSuccess, prevPostSuccess]);
 
     return (
         <AuthCard>
-            <AuthCard title="Forgot Password" />
+            <AuthHeader title="Forgot Password" />
 
             <Title>Forgot Password {showSuccess && 'Success'}</Title>
             {showSuccess ? (
@@ -28,12 +45,16 @@ const ForgotPassword: React.FC = () => {
                     link to reset your password.
                 </p>
             ) : (
-                <Form onSubmit={handleSubmit} isPosting={isPosting} error={error}>
+                <Form
+                    onSubmit={() => dispatch(postSendForgotPassword(formState.email))}
+                    isPosting={isPosting}
+                    error={error}
+                >
                     <FormRow>
                         <TextInput
                             name="email"
                             type="email"
-                            value={email}
+                            value={formState.email}
                             label="Email address"
                             onChange={handleChange}
                             required
