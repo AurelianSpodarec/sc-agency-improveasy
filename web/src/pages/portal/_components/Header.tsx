4d964cca6bdfc950ddd1e88f19df9b580b467051
Seb Smith <seb@silverchip.com>
2022-09-15 08:03:00 +0100

    Removes modal context from create

--- web/src/pages/portal/_components/Header.tsx
+++ web/src/pages/portal/_components/Header.tsx
@@ -1,7 +1,4 @@
-import { SyntheticEvent } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-
-import useModal from './../../../context/useModal';
+import { Link } from 'react-router-dom';
 
 import IconLogoPortal from '@content/icons/logo/IconLogoPortal';
 
@@ -10,53 +7,6 @@ import ActionButton from 'lib/src/components/button/ActionButton';
 import { Container } from '@components/ui';
 
 const Header = () => {
-    const modalContext = useModal();
-
-    function handlerCreateProperty(e: any) {
-        //@ts-ignore
-        e.preventDefault(); //@ts-ignore
-        modalContext.setConfig({
-            // id,
-            type: 'createProperty',
-            option: 'createProperty',
-            description: 'Hiskdjskdjsdksjdi',
-            inputs: [
-                {
-                    col: 'w-full',
-                    name: 'email',
-                    type: 'email',
-                    placeholder: 'Email',
-                },
-                {
-                    col: 'w-full',
-                    name: 'firstName',
-                    type: 'text',
-                    placeholder: 'First Name',
-                },
-                {
-                    col: 'w-full',
-                    name: 'lastName',
-                    type: 'text',
-                    placeholder: 'Last Name',
-                },
-                {
-                    col: 'w-full',
-                    name: 'password',
-                    type: 'password',
-                    placeholder: 'Password',
-                },
-                {
-                    col: 'w-full',
-                    name: 'confirmPassword',
-                    type: 'password',
-                    placeholder: 'Confirm Password',
-                },
-            ], // @ts-ignore
-            onAction: (...args) => createProperty(...args),
-        }); // @ts-ignore
-        modalContext.open('createProperty');
-    }
-
     return (
         <header className="header header--portal">
             <Container style={{ height: '100%' }}>
@@ -72,7 +22,7 @@ const Header = () => {
 
                     <div className="flex-row align-center">
                         <ActionButton
-                            onClick={e => handlerCreateProperty(e)}
+                            onClick={() => console.log('open modal')}
                             className="winged"
                             icon="home"
                         >
