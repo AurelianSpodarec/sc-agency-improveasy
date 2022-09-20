ef149c8d2071d348c1c999dd84449ead796eca2b
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-09-20 12:04:38 +0100

    update/(auth) - Updated overlay opacity and added 'login' button on confirm page

--- web/src/pages/auth/ConfirmEmail/ConfirmEmail.tsx
+++ web/src/pages/auth/ConfirmEmail/ConfirmEmail.tsx
@@ -3,6 +3,8 @@ import Form from 'lib/src/components/form/Form';
 import useConfirmEmail from './hooks/useConfirmEmail';
 
 import { AuthCard, AuthHeader, AuthSection } from '../_components';
+import ActionButton from 'lib/src/components/button/ActionButton';
+import LinkButton from 'lib/src/components/button/LinkButton';
 
 const ConfirmEmail: React.FC = () => {
     const { isPosting, error } = useConfirmEmail();
@@ -12,12 +14,20 @@ const ConfirmEmail: React.FC = () => {
             <AuthHeader title="Confirm Email" />
             Welcome to Improveasy!
             <AuthSection>
-                <Form onSubmit={() => {}} isPosting={isPosting} error={error} omitButtons>
-                    {isPosting && <i className="fal fa-spinner fa-spin"></i>}
-                </Form>
-                <div>
-                    We created an account for you. Please confirm your e-mail addrss and use our
-                    service to the maximum
+                <div style={{ marginBottom: '20px' }}>
+                    <Form onSubmit={() => {}} isPosting={isPosting} error={error} omitButtons>
+                        {isPosting && <i className="fal fa-spinner fa-spin"></i>}
+                    </Form>
+                    <div>
+                        We created an account for you. Please confirm your e-mail addrss and use our
+                        service to the maximum
+                    </div>
+                </div>
+                <div className="d-flex justify-between">
+                    <div></div>
+                    <LinkButton href="/auth/login" className="winged">
+                        Login
+                    </LinkButton>
                 </div>
             </AuthSection>
         </AuthCard>
