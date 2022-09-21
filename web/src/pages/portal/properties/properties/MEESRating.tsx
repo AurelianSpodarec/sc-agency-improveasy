f8a1e4043818c0ee45747e0edabf13d9d97c11c4
Seb Smith <seb@silverchip.com>
2022-09-21 08:59:37 +0100

    Tweaks mees icons

--- web/src/pages/portal/properties/properties/MEESRating.tsx
+++ web/src/pages/portal/properties/properties/MEESRating.tsx
@@ -1,26 +1,26 @@
 const MeesRating = ({ mees }: IProps) => {
     return mees ? (
-        <span className="inline-flex items-center justify-center h-6 w-6 rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+        <span className="inline-flex items-center justify-center h-8 w-8 rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-4 h-4"
+                strokeWidth={3}
+                stroke="green"
+                className="w-8 h-8"
             >
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
             </svg>
         </span>
     ) : (
-        <span className="inline-flex items-center justify-center h-6 w-6 rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800">
+        <span className="inline-flex items-center justify-center h-8 w-8 rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-4 h-4"
+                strokeWidth={3}
+                stroke="red"
+                className="w-8 h-8"
             >
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
