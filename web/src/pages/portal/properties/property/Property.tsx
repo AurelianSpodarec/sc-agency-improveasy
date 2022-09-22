72f8f789644661f6946210e2f6700e9450f13de3
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-09-23 00:46:34 +0100

    update: added icons and alignd them on the button

--- web/src/pages/portal/properties/property/Property.tsx
+++ web/src/pages/portal/properties/property/Property.tsx
@@ -12,6 +12,8 @@ import { fetchPropertyByID } from '@actions/properties/fetchPropertyByID';
 import { fetchAccountDetails } from '@actions/account/fetchAccountDetails';
 import { fetchPropertyEPCRating } from '@actions/propertyInformation/fetchPropertyEPCRating';
 
+import house from './../../../../_content/icons/house_outline_green.png';
+
 function Property() {
     const dispatch = useDispatch();
 
@@ -32,10 +34,12 @@ function Property() {
             <Section>
                 <div className="d-flex space-x-12">
                     <div className="hidden lg:block">
-                        <div className="properties-back-button">
-                            <div className="d-flex items-center space-x-4">
-                                <Link to="/portal/properties">Back to listing</Link>
+                        <Link className="properties-back" to="/portal/properties">
+                            <div className="properties-back__wrap d-flex items-center space-x-4">
+                                <img className="properties-back__house" src={house} alt="house" />
+                                <span className="properties-back__text">Home</span>
                                 <svg
+                                    className="properties-back__chevron"
                                     xmlns="http://www.w3.org/2000/svg"
                                     width="20px"
                                     viewBox="0 0 384 512"
@@ -43,8 +47,9 @@ function Property() {
                                     <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
                                 </svg>
                             </div>
-                        </div>
+                        </Link>
                     </div>
+
                     <MainCard
                         title={`${property?.addressLine1}, ${property?.postcode}`}
                         className="w-10/12"
