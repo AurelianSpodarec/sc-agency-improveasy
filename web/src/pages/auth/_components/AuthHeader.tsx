bacb2fe7de650768e8780ea8800396c011bce6c2
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-09-28 10:45:06 +0100

    update: login header and added a new modifier for margin bottom

--- web/src/pages/auth/_components/AuthHeader.tsx
+++ web/src/pages/auth/_components/AuthHeader.tsx
@@ -1,16 +1,11 @@
 // import { IconLogoPublic } from '@content/icons/logo/IconLogoPublic';
 
-import { Text } from '@components/ui';
+import { PageHeading, Text } from '@components/ui';
 
 function AuthHeader({ children, title }: any) {
     return (
         <header className="authLogin__header">
-            {/* <div style={{ width: '100px' }}>
-                <IconLogoPublic />
-            </div> */}
-            <Text size="xl" type="h2" weight="bold">
-                {title}
-            </Text>
+            <PageHeading title={title} space="sm" />
             {children}
         </header>
     );
